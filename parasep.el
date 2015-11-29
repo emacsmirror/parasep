f0ad27ac5fd39faf3638a1fb7aa845af6bf6428c
Kevin Ryde <user42_kevin@yahoo.com.au>
2015-11-29 17:30:59 +0100

    updates

--- parasep.el
+++ parasep.el
@@ -3,7 +3,7 @@
 ;; Copyright 2007, 2008, 2009, 2010, 2011, 2012, 2015 Kevin Ryde
 
 ;; Author: Kevin Ryde <user42_kevin@yahoo.com.au>
-;; Version: 5
+;; Version: 6
 ;; Keywords: convenience, paragraphs
 ;; URL: http://user42.tuxfamily.org/parasep/index.html
 
@@ -29,6 +29,7 @@
 ;;     parasep-empty-comments  empty comments separator
 ;;     parasep-perl-pod        pod =foo command separators
 ;;     parasep-perl-pod-X      X<> index directive separator
+;;     parasep-tex-index       \index{} on a line alone
 ;;     parasep-texinfo-@*      @* line break separator
 ;;
 ;; The functions are designed to be used either from a mode hook, or
@@ -63,17 +64,20 @@
 ;; Version 3 - correction to custom-add-option
 ;; Version 4 - new parasep-perl-pod-X
 ;; Version 5 - new email
+;; Version 6 - new parasep-tex-index
 
 ;;; Code:
 
 (defun parasep-regexp-valid-p (str)
-  "Return non-nil if string STR is a valid regexp."
+  "An internal part of parasep.el.
+Return non-nil if string STR is a valid regexp."
   (condition-case nil
       (progn (string-match str "") t)
     (invalid-regexp nil)))
 
 (defun parasep-regexp-split (regexp)
-  "Return a list of the toplevel alternates of REGEXP.
+  "An internal part of parasep.el.
+Return a list of the toplevel alternates of REGEXP.
 REGEXP is split on top-level \\=\\| so for instance
 \"a\\=\\|\\=\\(b\\=\\|c\\=\\)\" gives a list (\"a\" \"\\=\\(b\\=\\|c\\=\\)\").
 
@@ -90,10 +94,11 @@ no splitting past the bad point."
     ret))
 
 (defun parasep-add-to-regexp-var (var regexp)
-  "Extend regexp variable VAR to match REGEXP too.
+  "An internal part of parasep.el.
+Extend regexp variable VAR to match REGEXP too.
 VAR is a symbol, the name of a variable containing a regexp
-string.  If REGEXP is not already among the toplevel alternates
-in VAR then it's prepended.
+string.  If the given REGEXP is not already among the toplevel
+alternates in VAR then it's prepended.
 
 The return is the new value of VAR."
 
@@ -132,7 +137,11 @@ Or a comment line of dashes similarly.
 
 The pattern added is quite loose, simply an optional
 `comment-start-skip' followed by 4 or more \"-\".  Perhaps in the
-future this may have to be a bit tighter."
+future this may have to be a bit tighter.
+
+The current value of `comment-start-skip' is copied into
+`paragraph-start' and `paragraph-separate' etc, so if customizing
+`comment-start-skip' be sure to do that before `parasep-dashes'."
 
   (interactive)
   (parasep-add (concat
@@ -243,7 +252,6 @@ together with the text and the style written is preserved.
     blah blah."
 
   (interactive)
-  (require 'parasep)
   (parasep-add "\\(X<[^>\n]*>\\s-*\\)+$"))
 
 ;;;###autoload
@@ -255,6 +263,40 @@ together with the text and the style written is preserved.
 
 
 ;;-----------------------------------------------------------------------------
+;; TeX
+
+;;;###autoload
+(defun parasep-tex-index ()
+  "Make \\=\\index{} on a line alone a paragraph separator.
+This is designed to prevent a \\=\\index entry on a line alone
+from being included in an adjacent paragraph when filling etc.
+
+    \\=\\index{Foo|(}
+    Foo is blah blah blah ...
+    \\=\\index{Foo|)}
+
+If \\=\\index is within text, not on a line of its own, then it's
+not a separator and is filled in the usual way.
+
+    Foo\\=\\index{Foo} is blah blah blah ...
+
+An \\=\\index can contain braces, but no more than one nested
+level.  This allows a little TeX for a dual plain and formatted
+style index entry.
+
+    \\=\\index{Some-thing@Some\\=\\hyp{}thing}
+
+\\=\\index is usually for LaTeX but with some packages the same
+can apparently be used in other flavours, including plain TeX."
+
+  (interactive)
+  (parasep-add "\\\\index{[^{}]*\\({[^{}]*}[^{}]*\\)*}[ \t]*\\(%.*\\)?$"))
+
+;;;###autoload
+(custom-add-option 'latex-mode-hook 'parasep-tex-index)
+
+;;-----------------------------------------------------------------------------
+;; Texinfo
 
 ;;;###autoload
 (defun parasep-texinfo-@* ()
@@ -266,7 +308,7 @@ The effect is that paragraph filling won't flow an @* into
 surrounding lines, so the texinfo source keeps a line break
 similar to what @* will produce in the formatted output.
 
-\(As of Emacs 23.1, the default settings have all alphabetical
+\(As of Emacs 24.5, the default settings have alphabetical
 @foo commands as paragraph separators, but not @*.)"
 
   (interactive)
